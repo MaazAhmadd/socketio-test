1217951968da23cab38986e07f01ec7294d89202
maazahmadd <maaz.ahmad213@gmail.com>
2024-04-03 04:25:19 +0500

    updatess

--- server/prisma/migrations/20240402232428_init/migration.sql
+++ server/prisma/migrations/20240402232428_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "Member" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT,
     "handle" TEXT NOT NULL,
     "profilePicture" TEXT,
@@ -9,32 +9,37 @@ CREATE TABLE "Member" (
     "micEnabled" BOOLEAN NOT NULL,
     "leaderPriorityCounter" INTEGER NOT NULL,
     "roomId" TEXT,
-    CONSTRAINT "Member_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Member_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "VideoPlayer" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "isPlaying" BOOLEAN NOT NULL,
     "source" TEXT NOT NULL,
     "totalDuration" INTEGER NOT NULL,
     "playedTill" INTEGER NOT NULL,
     "roomId" TEXT NOT NULL,
-    CONSTRAINT "VideoPlayer_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "VideoPlayer_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Room" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "status" TEXT NOT NULL DEFAULT 'Public'
+    "id" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'Public',
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Kicked" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "memberHandle" TEXT NOT NULL,
     "roomId" TEXT NOT NULL,
-    CONSTRAINT "Kicked_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Kicked_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -45,3 +50,12 @@ CREATE INDEX "Member_handle_idx" ON "Member"("handle");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "VideoPlayer_roomId_key" ON "VideoPlayer"("roomId");
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoPlayer" ADD CONSTRAINT "VideoPlayer_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Kicked" ADD CONSTRAINT "Kicked_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
