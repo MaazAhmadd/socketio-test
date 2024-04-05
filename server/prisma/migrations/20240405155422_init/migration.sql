49e86472a705743d8b7440e8da2fa50ffba85750
maazahmadd <maaz.ahmad213@gmail.com>
2024-04-05 20:55:08 +0500

    switch to postgres

--- server/prisma/migrations/20240405155422_init/migration.sql
+++ server/prisma/migrations/20240405155422_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "Member" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT,
     "handle" TEXT NOT NULL,
     "profilePicture" TEXT,
@@ -9,12 +9,13 @@ CREATE TABLE "Member" (
     "micEnabled" BOOLEAN NOT NULL,
     "leaderPriorityCounter" INTEGER NOT NULL,
     "roomId" TEXT,
-    CONSTRAINT "Member_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Member_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "VideoPlayer" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "isPlaying" BOOLEAN NOT NULL,
     "sourceUrl" TEXT NOT NULL,
     "thumbnailUrl" TEXT NOT NULL,
@@ -22,30 +23,36 @@ CREATE TABLE "VideoPlayer" (
     "totalDuration" INTEGER NOT NULL,
     "playedTill" INTEGER NOT NULL,
     "roomId" TEXT NOT NULL,
-    CONSTRAINT "VideoPlayer_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "VideoPlayer_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Room" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "status" TEXT NOT NULL DEFAULT 'Public'
+    "id" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'Public',
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Kicked" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "memberHandle" TEXT NOT NULL,
     "roomId" TEXT NOT NULL,
-    CONSTRAINT "Kicked_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Kicked_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "YtVideo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "ytId" TEXT NOT NULL,
     "thumbnail" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "YtVideo_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -56,3 +63,12 @@ CREATE UNIQUE INDEX "VideoPlayer_roomId_key" ON "VideoPlayer"("roomId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "YtVideo_ytId_key" ON "YtVideo"("ytId");
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoPlayer" ADD CONSTRAINT "VideoPlayer_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Kicked" ADD CONSTRAINT "Kicked_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
