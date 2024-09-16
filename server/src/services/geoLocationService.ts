7685b36e29fcd995e109adc5164abf642490dc68
maazahmadd <maaz.ahmad213@gmail.com>
2024-09-16 15:43:53 +0500

    updates

--- server/src/services/geoLocationService.ts
+++ server/src/services/geoLocationService.ts
@@ -1,18 +1,23 @@
 import maxmind, { CountryResponse } from "maxmind";
+import { logger } from "../logger";
 
 // Function to get country from IP address
 export const getCountryFromIP = async (ip: string): Promise<string> => {
+	logger.info(`getCountryFromIP, ip: ${ip}`);
 	if (ip == "::1" || ip.includes("127.0.0.1")) {
+		logger.info("getCountryFromIP, country: PK");
 		return "PK";
 	}
 	const lookup = await maxmind.open<CountryResponse>(
-		"./geo-database/GeoLite2-Country.mmdb",
+		"geo-database/GeoLite2-Country.mmdb",
 	);
 
 	const result = lookup.get(ip);
 
 	if (result?.country) {
+		logger.info(`getCountryFromIP, country: ${result.country.iso_code}`);
 		return result.country.iso_code;
 	}
+	logger.info("getCountryFromIP, country: 0");
 	return "0";
 };
